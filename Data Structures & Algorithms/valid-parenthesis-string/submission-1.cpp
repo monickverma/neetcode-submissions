330293fe09bd0eabc06a9dbcd9d7080fa7f80e25
Monick Verma <35963671+monickverma@users.noreply.github.com>
2026-04-30 02:28:34 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int high=0,low=0;
+
+        for(char c:s){
+            if (c=='(') low++,high++;
+            else if (c==')')low--,high--;
+            else low--,high++;
+
+            if(low<0)low=0;
+            else if (high<0)return false;
+        }
+        return low==0;
+    }
+};
